602cfa21bd8c394052cb9f8f2df049747117085b
Pirmin Grob <pirmin.grob@bluewin.ch>
2017-03-24 14:29:17 +0100

    added main loop structure

--- Sources/program.c
+++ Sources/program.c
@@ -7,8 +7,38 @@
 
 #include "program.h"
 
+/*
+ * global variables for main loop
+ */
+volatile enum parcoursState state;
+volatile enum parcoursState stateBak;
+volatile enum parcoursState stateCurr;
+volatile enum wheelPosition wheelPos;
+volatile enum parcourType type;
+volatile enum parcourEnd endReached;
+volatile enum driveDistance distance;
+/*
+ * global variables for button pressing
+ */
+volatile uint8_t button;
+volatile enum buttonChecked btnchk;
+
 void start(void){
-	simulateParcour();
+	/* init variables */
+	state = STOPPED;
+	stateBak = STOPPED;
+	stateCurr = STOPPED;
+	wheelPos = STRAIGHT; // todo: init servos for straight wheels
+	type = PARCOUR_A; // todo: check switch: type = value of switch port
+	endReached = NOT_REACHED;
+	distance = CURVE_DIST;
+	button = BUTTON3;
+	btnchk = BUTTON_UNCHECKED;
+	/* temporary simulation of parcour to test serial interface protocol */
+	//simulateParcour();
+	/* start mainloop */
+	mainLoop();
+	/* fallback loop, should never be reached */
 	for(;;);
 }
 
@@ -16,19 +46,32 @@ void serialRxInt(uint8_t ch, uint8_t port){
 	switch (ch){
 	case START:
 		serialSend(ACKNOWLEDGE, port);
-		// start driving
+		if (state == STOPPED)
+			state = DRIVE_FORWARD;
 		break;
 	case PAUSE:
 		serialSend(ACKNOWLEDGE, port);
-		// stop
+		stateBak = state;
+		state = STOPPED;
 		break;
 	case RESUME:
 		serialSend(ACKNOWLEDGE, port);
-		// start driving again
+		state = stateBak;
 		break;
 	case ACKNOWLEDGE:
 	case ERROR:
 		break;
+	case BUTTON1:
+	case BUTTON2:
+	case BUTTON3:
+	case BUTTON4:
+	case BUTTON5:
+		serialSend(ACKNOWLEDGE, port);
+		button = ch;
+		btnchk = BUTTON_CHECKED;
+		break;
+	case ROMAN_NUMERAL_REQUEST:
+	case CURVE:
 	default:
 		serialSend(ERROR, port);
 	}
@@ -42,8 +85,6 @@ void serialSend(uint8_t ch, uint8_t port){
 	case RasPi:
 		CLS2_SendChar(ch);
 		break;
-	//default:
-		// error
 	}
 }
 
@@ -54,4 +95,56 @@ void simulateParcour(void){
 	WAIT1_Waitms(2000);
 	serialSend(ROMAN_NUMERAL_REQUEST, PC);
 	serialSend(ROMAN_NUMERAL_REQUEST, RasPi);
+}
+
+void mainLoop(void) {
+	for (;;) {
+		if (stateCurr != state) { // todo: set servo position and motor direction and speed (check also wheelPos)
+			switch (state) {
+			case STOPPED: /* do nothing */
+				break;
+			case DRIVE_FORWARD:
+			case DRIVE_BACKWARD:
+				// if wheelPos != STRAIGHT, turn servos, change wheelPos, wait
+				// set motor directions (check forward/backwards) and speeds
+				break;
+			case DRIVE_LEFT:
+			case DRIVE_RIGHT:
+				// if wheelPos != SIDEWAYS, turn servos, change wheelPos, wait
+				// set motor directions (check left/right) and speeds
+				// set distance
+				break;
+			}
+			stateCurr = state;
+		} else { // todo: control the rigth direction with ToF sensors
+			switch (state) { // detect changes of parcours and change state
+			case STOPPED: /* do nothing */
+				break;
+			case DRIVE_FORWARD:
+				// right sensor front (?) to short distance, decrease motor speed left for defined time
+				// detect end, change to drive left or right mode depending on parcour type
+					// send signal curve to raspi
+				break;
+			case DRIVE_BACKWARD:
+				// if end reached, drive fixed distance into button, else:
+					// right sensor front (?) to short distance, decrease motor speed left for defined time
+					// detect end and change to drive left or right for button pressing depending on parcour type
+						// set endReached to reached
+						// ask raspi for roman number
+						// wait, to give raspi time to answer
+				break;
+			case DRIVE_LEFT:
+				// right sensor front - right sensor back > 0 ==> decrease motor speed front for defined time
+				// drive until target distance is reached
+					// change to drive backward mode
+				break;
+			case DRIVE_RIGHT:
+				// left sensor front - left sensor back > 0 ==> decrease motor speed front for defined time
+				// drive until target distance is reached
+					// change to drive backward mode
+				break;
+			}
+		}
+		WAIT1_Waitms(10);
+	}
 }
--- Sources/program.h
+++ Sources/program.h
@@ -24,7 +24,12 @@ enum serialPort
 enum serialResponse
 {
 	ACKNOWLEDGE = 0x20,
-	ERROR = 0x10
+	ERROR = 0x10,
+	BUTTON1 = 0x01,
+	BUTTON2 = 0x02,
+	BUTTON3 = 0x03,
+	BUTTON4 = 0x04,
+	BUTTON5 = 0x05
 };
 enum serialCommands // !!!ERROR!!! not every value may be used, serial Rx interrupt isn't called with every byte
 {
@@ -36,11 +41,58 @@ enum serialCommands // !!!ERROR!!! not every value may be used, serial Rx interr
 };
 
 /*
+ * Declaration of states for main loop
+ */
+enum parcoursState
+{
+	DRIVE_FORWARD,
+	DRIVE_BACKWARD,
+	DRIVE_LEFT,
+	DRIVE_RIGHT,
+	STOPPED
+};
+enum parcourType
+{
+	PARCOUR_A = 0x00,
+	PARCOUR_B = 0x01
+};
+enum buttonChecked
+{
+	BUTTON_CHECKED,
+	BUTTON_UNCHECKED
+};
+enum wheelPosition
+{
+	STRAIGHT,
+	SIDEWAYS
+};
+enum parcourEnd
+{
+	REACHED,
+	NOT_REACHED
+};
+enum driveDistance
+{
+	CURVE_DIST = 0xA0, // todo: change values
+	BUTTON1_A = 0xA0,
+	BUTTON2_A = 0xA0,
+	BUTTON3_A = 0xA0,
+	BUTTON4_A = 0xA0,
+	BUTTON5_A = 0xA0,
+	BUTTON1_B = 0xA0,
+	BUTTON2_B = 0xA0,
+	BUTTON3_B = 0xA0,
+	BUTTON4_B = 0xA0,
+	BUTTON5_B = 0xA0,
+};
+
+/*
  * program.c
  */
 void start(void); // init method
 void serialRxInt(uint8_t ch, uint8_t port); // called from Hardware Interrupt
 void serialSend(uint8_t ch, uint8_t port); // sends byte to serial port
 void simulateParcour(void); // method to simulate the serial communication of a complete parcour
+void mainLoop(void); // main loop for driving
 
 #endif /* SOURCES_PROGRAM_H_ */
