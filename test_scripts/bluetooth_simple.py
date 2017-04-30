e2964fc93c49e0794da584e3ec4726def1ccae63
Pirmin Grob <pirmin.grob@bluewin.ch>
2017-04-30 19:07:21 +0200

    bt test scripts

---
+++ test_scripts/bluetooth_simple.py
@@ -0,0 +1,30 @@
+import serial
+import threading
+
+s=serial.Serial(
+        port='/dev/rfcomm0',
+        baudrate=9600,
+        parity=serial.PARITY_EVEN,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS
+)
+
+s.isOpen()
+
+def writeByteToSerial(data):
+    s.write(data)
+
+def printByte():
+    while True:
+        print('FD: '+str(s.read(1)))
+
+t=threading.Thread(target=printByte)
+t.daemon=True
+t.start()
+
+# Start with:
+#python -i <name.py> # -> python3
+
+# Example:
+#writeByteToSerial(b'\x80')
+
